b4a45a32c2945a210cb759cc38fee68ea48ef4db
Niraj7667 <143164430+Niraj7667@users.noreply.github.com>
2024-08-19 03:08:34 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import Hero from './components/hero/Hero.jsx'; // Update path as needed
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Hero />
   </React.StrictMode>
 );
