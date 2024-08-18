888a44bc575c445f27f07d4d7b7dc2650c8020ee
Niraj7667 <143164430+Niraj7667@users.noreply.github.com>
2024-08-19 02:29:51 +0530

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../navbar/Navbar.jsx"
 import HomePage from "../HomePage/HomePage.jsx"
+import Login from "../Login/Login.jsx"
 function Hero() {
   return (
     <div>
       <Navbar/>
       <HomePage/>
+      <Login/>
     </div>
   )
 }
