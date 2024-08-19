c6c3d65b9c82eef3dcb1665805410b56784ca8b4
Niraj7667 <143164430+Niraj7667@users.noreply.github.com>
2024-08-19 10:16:08 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Hero from './components/hero/Hero.jsx'; // Update path as needed
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,RouterProvider
+} from "react-router-dom";
+import ItemCard from './components/item-card/ItemCard.jsx'
+import Profile from './components/profile/Profile.jsx';
+import Login from './components/Login/Login.jsx';
+import Signup from './components/Signup/Signup.jsx';
+import Logout from './components/logout/Logout.jsx';
+import SharedCart from './components/CreateSharedCart/CreateSharedCart.jsx';
+import JoinCart from './components/JoinCart/JoinCart.jsx';
+import HomePage from './components/HomePage/HomePage.jsx';
+ import SharedCartPage from './components/pages/SharedCartPage.jsx';
+ import SetCardId from './components/cartIdSet/SetCartId.jsx';
+const router = createBrowserRouter([
+  { path: "/",element: <App />,},
+  { path: "/cards", element: <ItemCard/>},
+  { path: "/profile", element: <Profile/>},
+  { path: "/login", element: <Login />},
+  { path: "/signup", element: <Signup/>},
+  { path: "/logout", element: <Logout/>},
+  { path: "/sharedcart", element: <SharedCart/>},
+  { path: "/joincart", element: <JoinCart/>},
+  { path: "/products", element: <HomePage/>},
+  { path: "/carted", element: <SharedCartPage/>},
+  { path: "/setcartid", element: <SetCardId/>},
+  
+// { path: "/user/sharedcarts", element: <UserSharedCartsPage/>},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Hero />
-  </React.StrictMode>
-);
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
