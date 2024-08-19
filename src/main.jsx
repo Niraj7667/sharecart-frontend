aa002b0528eb2fe033f4a097f1d91d08b398ec3b
Niraj7667 <143164430+Niraj7667@users.noreply.github.com>
2024-08-19 11:39:37 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {
-  createBrowserRouter,RouterProvider
-} from "react-router-dom";
-import ItemCard from './components/item-card/ItemCard.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ItemCard from './components/item-card/ItemCard.jsx';
 import Profile from './components/profile/Profile.jsx';
 import Login from './components/Login/Login.jsx';
 import Signup from './components/Signup/Signup.jsx';
@@ -13,26 +11,31 @@ import Logout from './components/logout/Logout.jsx';
 import SharedCart from './components/CreateSharedCart/CreateSharedCart.jsx';
 import JoinCart from './components/JoinCart/JoinCart.jsx';
 import HomePage from './components/HomePage/HomePage.jsx';
- import SharedCartPage from './components/pages/SharedCartPage.jsx';
- import SetCardId from './components/cartIdSet/SetCartId.jsx';
-const router = createBrowserRouter([
-  { path: "/",element: <App />,},
-  { path: "/cards", element: <ItemCard/>},
-  { path: "/profile", element: <Profile/>},
-  { path: "/login", element: <Login />},
-  { path: "/signup", element: <Signup/>},
-  { path: "/logout", element: <Logout/>},
-  { path: "/sharedcart", element: <SharedCart/>},
-  { path: "/joincart", element: <JoinCart/>},
-  { path: "/products", element: <HomePage/>},
-  { path: "/carted", element: <SharedCartPage/>},
-  { path: "/setcartid", element: <SetCardId/>},
-  
-// { path: "/user/sharedcarts", element: <UserSharedCartsPage/>},
-]);
+import SharedCartPage from './components/pages/SharedCartPage.jsx';
+import SetCardId from './components/cartIdSet/SetCartId.jsx';
+
+function MainRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/cards" element={<ItemCard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/sharedcart" element={<SharedCart />} />
+        <Route path="/joincart" element={<JoinCart />} />
+        <Route path="/products" element={<HomePage />} />
+        <Route path="/carted" element={<SharedCartPage />} />
+        <Route path="/setcartid" element={<SetCardId />} />
+      </Routes>
+    </Router>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <MainRouter />
   </React.StrictMode>,
-)
+);
